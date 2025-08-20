aae9ddb7a6b7eea36559713cf77c2d83e2d32145
martifu <martin0013esp@hotmail.com>
2025-08-20 10:08:40 -0600

    fix maximumFileSizeToCacheInBytes

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB
         runtimeCaching: [
           {
             urlPattern: ({ request }) => request.mode === 'navigate',
